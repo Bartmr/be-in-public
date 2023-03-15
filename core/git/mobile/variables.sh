2caf153137ea23c52b2060135bef70d9bd908cdc
Bartolomeu <pbartmr@gmail.com>
2023-03-15 13:04:39 +0000

    Commit

--- core/git/mobile/variables.sh
+++ core/git/mobile/variables.sh
@@ -1,4 +1,4 @@
 
-source /data/data/com.termux/files/home/storage/shared/Documents/repositories/core/git/variables.sh
+source /data/data/com.termux/files/home/storage/shared/Documents/repositories/my-notes/core/git/variables.sh
 
 important_repos_dir="/data/data/com.termux/files/home/storage/shared/Documents/repositories"
