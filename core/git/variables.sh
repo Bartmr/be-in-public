6cfc31fd7b87f38837b58553bb2a36e4a16f6667
Bartolomeu <pbartmr@gmail.com>
2023-03-15 12:54:07 +0000

    Commit

--- core/git/variables.sh
+++ core/git/variables.sh
@@ -1,4 +1,4 @@
-source "$HOME/Documents/repositories/my-notes/core/git/important-repos.sh"
+source "$HOME/Documents/repositories/my-notes/core/git/variables.sh"
 
 important_repos_dir="$HOME/Documents/repositories"
 
