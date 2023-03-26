be4753c5b0657f45b3915babaeb15a744bd7e85f
Bartolomeu <pbartmr@gmail.com>
2023-03-26 22:23:28 +0100

    Commit

--- core/git/check-repos-branch-protection.sh
+++ core/git/check-repos-branch-protection.sh
@@ -34,37 +34,18 @@ source ./tokens.ignore.sh
 #
 #
 
+parallel_args=()
 
 for repo in "${important_repos[@]}"
 do
 
   branch="main"
   
-  branch_protection_result=$(curl \
-    --silent \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $github_token"\
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/${github_username}/$repo/branches/$branch/protection)
-
-: '
-sed commands replace the template strings with the actual values
-'
-  expected_branch_protection=$(cat expected-github-branch-protection.json \
-    | sed "s/\${username}/$github_username/" | sed "s/\${repo}/$repo/" | sed "s/\${branch}/$branch/"
-  )
-
-  if ! [ "$branch_protection_result" == "$expected_branch_protection" ]
-  then
-    echo -e "${danger_colors}INVALID BRANCH PROTECTION \
-FOR REPOSITORY ${repo}, BRANCH ${branch}:"
-
-    diff --color <(echo "$expected_branch_protection") <(echo "$branch_protection_result")
-
-    exit 1
-  fi
+  parallel_args+=("./_check-repos-branch-protection/check-branch-protection-for-repo.sh ${repo} ${branch}")
 
 done
+
+printf '%s\n' "${parallel_args[@]}" | parallel --jobs 2 --halt soon,fail=1 --keep-order
 
 echo -e "
 
