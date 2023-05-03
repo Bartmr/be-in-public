334d963e856d6bfa0ac3e66abcad895ee4c1bfe6
Bartolomeu <pbartmr@gmail.com>
2023-05-03 22:14:18 +0100

    commit

--- core/git/mobile/variables.sh
+++ core/git/mobile/variables.sh
@@ -1,4 +1,10 @@
 
+if ! [ -x "$(command -v jq)" ]; then
+  # Install the executable
+  apt-get update
+  apt-get install jq
+fi
+
 source /data/data/com.termux/files/home/storage/shared/Documents/repositories/my-notes/core/git/variables.sh
 
 important_repos_dir="/data/data/com.termux/files/home/storage/shared/Documents/repositories"
