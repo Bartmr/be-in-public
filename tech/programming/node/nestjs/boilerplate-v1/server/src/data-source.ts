da7eeba55d0b506bfe1d3a120bce56c4289a7f36
Bartolomeu <pbartmr@gmail.com>
2023-05-11 11:04:17 +0100

    commit

--- tech/programming/node/nestjs/boilerplate-v1/server/src/data-source.ts
+++
@@ -1,9 +0,0 @@
-import './internals/load-environment-variables.ts';
-
-import { DataSource } from 'typeorm';
-import { APP_DATA_SOURCE_CONFIG } from './data-source-config';
-
-export const AppDataSource = new DataSource({
-  ...APP_DATA_SOURCE_CONFIG,
-  entities: ['src/**/*.entity.ts'],
-});
--- tech/programming/node/nestjs/boilerplate-v1/start-dev-infrastructure.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-this_dir=$(dirname "$(realpath $0)")
-cd "$this_dir"
-
-docker compose -f ./infrastructure/docker-compose.dev.yml up
